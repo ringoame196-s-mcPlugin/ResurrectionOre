bf355508902a2f8ebbf3ea109fefc9b32c5f3008
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-10-08 16:07:50 +0900

    ✨ ブロックの復活時間を管理するファイル関係

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ResurrectionTimeManager.kt
@@ -0,0 +1,28 @@
+package com.github.ringoame196_s_mcPlugin
+
+import org.bukkit.Material
+import java.io.File
+
+object ResurrectionTimeManager {
+	private val resurrectionTimeData = mutableMapOf<Material, Int>()
+
+	fun load(file: File) {
+		val yamlFileManager = YamlFileManager()
+		reset()
+		val fileData = yamlFileManager.loadYAsMap(file)
+
+		for ((k,v) in fileData) {
+			val blockType = Material.getMaterial(k) ?: continue
+			val time = v.toIntOrNull() ?: continue
+			resurrectionTimeData[blockType] = time
+		}
+	}
+
+	fun getResurrectionTime(blockType: Material): Int? {
+		return resurrectionTimeData[blockType]
+	}
+
+	private fun reset() {
+		resurrectionTimeData.clear()
+	}
+}
