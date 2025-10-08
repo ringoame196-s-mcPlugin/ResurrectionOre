7de2bb8d933564a751df20312c7cad26eec35147
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-10-08 15:39:26 +0900

    ✨ イベント基礎実装

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/Events.kt
@@ -1,5 +1,10 @@
 package com.github.ringoame196_s_mcPlugin.events
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 
-class Events : Listener
+class Events : Listener {
+    @EventHandler
+    fun onBlockBreak(e: BlockBreakEvent) {}
+}
