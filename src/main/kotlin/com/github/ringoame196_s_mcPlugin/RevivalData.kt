e9f5db03e03e2e43f3538171cba602444a960bc7
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-10-08 17:54:42 +0900

    ✨  RevivalDataクラス追加

---
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/RevivalData.kt
@@ -0,0 +1,9 @@
+package com.github.ringoame196_s_mcPlugin
+
+import org.bukkit.Location
+import org.bukkit.Material
+
+data class RevivalData(
+    val blockType: Material,
+    val location: Location
+)
