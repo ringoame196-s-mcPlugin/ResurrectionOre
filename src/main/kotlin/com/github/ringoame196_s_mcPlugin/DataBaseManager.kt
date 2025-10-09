52ac6319b9b90bea2c71e0b6df90003ec18e7a66
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-10-09 14:13:56 +0900

    ♻️  リファクタリング

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/DataBaseManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/DataBaseManager.kt
@@ -7,82 +7,82 @@ import java.sql.PreparedStatement
 import java.sql.SQLException
 
 class DataBaseManager(private val dbFilePath: String) {
-	/**
-	 * SQLコマンドを実行する
-	 * @param command 実行するSQL文
-	 * @param parameters パラメータリスト
-	 */
-	fun executeUpdate(command: String, parameters: List<Any>? = null) {
-		try {
-			connection.use { conn ->
-				conn.prepareStatement(command).use { preparedStatement ->
-					parameters?.bindParameters(preparedStatement)
-					preparedStatement.executeUpdate()
-				}
-			}
-		} catch (e: SQLException) {
-			Bukkit.getLogger().info("SQL Error: ${e.message}")
-			throw e
-		}
-	}
+    /**
+     * SQLコマンドを実行する
+     * @param command 実行するSQL文
+     * @param parameters パラメータリスト
+     */
+    fun executeUpdate(command: String, parameters: List<Any>? = null) {
+        try {
+            connection.use { conn ->
+                conn.prepareStatement(command).use { preparedStatement ->
+                    parameters?.bindParameters(preparedStatement)
+                    preparedStatement.executeUpdate()
+                }
+            }
+        } catch (e: SQLException) {
+            Bukkit.getLogger().info("SQL Error: ${e.message}")
+            throw e
+        }
+    }
 
-	/**
-	 * 単一の値を取得する
-	 * @param sql 実行するSQL文
-	 * @param parameters パラメータリスト
-	 * @param label カラム名
-	 * @return 結果の値
-	 */
-	fun acquisitionValue(sql: String, parameters: List<Any>, label: String): Any? {
-		return acquisitionValues(sql, parameters, mutableListOf(label)).getValue(label)
-	}
+    /**
+     * 単一の値を取得する
+     * @param sql 実行するSQL文
+     * @param parameters パラメータリスト
+     * @param label カラム名
+     * @return 結果の値
+     */
+    fun acquisitionValue(sql: String, parameters: List<Any>, label: String): Any? {
+        return acquisitionValues(sql, parameters, mutableListOf(label)).getValue(label)
+    }
 
-	/**
-	 * 複数の行を取得する
-	 * @param sql 実行するSQL文
-	 * @param parameters パラメータリスト
-	 * @param mapper 結果セットの行をオブジェクトにマッピングする関数
-	 * @return 結果リスト
-	 */
-	fun acquisitionValues(
-		sql: String,
-		parameters: List<Any>,
-		keys: List<String>
-	): Map<String, Any?> {
-		try {
-			val values = mutableMapOf<String, Any?>()
-			connection.use { conn ->
-				conn.prepareStatement(sql).use { preparedStatement ->
-					parameters.bindParameters(preparedStatement)
-					preparedStatement.executeQuery().use { resultSet ->
-						if (resultSet.next()) {
-							for (key in keys) {
-								values[key] = try {
-									resultSet.getString(key)
-								} catch (e: SQLException) {
-									null
-								}
-							}
-						}
-					}
-				}
-			}
-			return values
-		} catch (e: SQLException) {
-			Bukkit.getLogger().info("SQL Error: ${e.message}")
-			throw e
-			return mapOf()
-		}
-	}
+    /**
+     * 複数の行を取得する
+     * @param sql 実行するSQL文
+     * @param parameters パラメータリスト
+     * @param mapper 結果セットの行をオブジェクトにマッピングする関数
+     * @return 結果リスト
+     */
+    fun acquisitionValues(
+        sql: String,
+        parameters: List<Any>,
+        keys: List<String>
+    ): Map<String, Any?> {
+        try {
+            val values = mutableMapOf<String, Any?>()
+            connection.use { conn ->
+                conn.prepareStatement(sql).use { preparedStatement ->
+                    parameters.bindParameters(preparedStatement)
+                    preparedStatement.executeQuery().use { resultSet ->
+                        if (resultSet.next()) {
+                            for (key in keys) {
+                                values[key] = try {
+                                    resultSet.getString(key)
+                                } catch (e: SQLException) {
+                                    null
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return values
+        } catch (e: SQLException) {
+            Bukkit.getLogger().info("SQL Error: ${e.message}")
+            throw e
+            return mapOf()
+        }
+    }
 
-	// SQLiteコネクションの取得
-	private val connection: Connection
-		get() = DriverManager.getConnection("jdbc:sqlite:$dbFilePath")
+    // SQLiteコネクションの取得
+    private val connection: Connection
+        get() = DriverManager.getConnection("jdbc:sqlite:$dbFilePath")
 
-	// パラメータをPreparedStatementにバインドする拡張関数
-	private fun List<Any>.bindParameters(preparedStatement: PreparedStatement) {
-		this.forEachIndexed { index, param ->
-			preparedStatement.setObject(index + 1, param)
-		}
-	}
+    // パラメータをPreparedStatementにバインドする拡張関数
+    private fun List<Any>.bindParameters(preparedStatement: PreparedStatement) {
+        this.forEachIndexed { index, param ->
+            preparedStatement.setObject(index + 1, param)
+        }
+    }
 }
