3f61b2bbdfd7b48580592498d0316fef15fb4405
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-10-10 13:16:10 +0900

    ✨ クリエイティブ時に破壊されると復活しないように

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/RevivalSchedule.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/RevivalSchedule.kt
@@ -1,21 +1,30 @@
 package com.github.ringoame196_s_mcPlugin
 
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.plugin.Plugin
+import org.bukkit.scheduler.BukkitTask
 
 object RevivalSchedule {
-    private val taskList = mutableListOf<RevivalData>()
+    private val taskList = mutableMapOf<Location, BukkitTask>()
 
     fun addTask(data: RevivalData, time: Int, plugin: Plugin) {
-        if (taskList.contains(data)) return
-        taskList.add(data)
-        Bukkit.getScheduler().runTaskLater(
+        val location = data.location
+        if (taskList.contains(location)) return
+        val task = Bukkit.getScheduler().runTaskLater(
             plugin,
             Runnable { // 実行したいコードをここに書く
                 RevivalManager.resurrectionBlock(data)
-                taskList.remove(data)
+                taskList.remove(data.location)
             },
             (time * 20).toLong()
         ) // 20Lは1秒を表す（1秒 = 20ticks）
+        taskList[data.location] = task
+    }
+
+    fun removeTask(location: Location) {
+        val task = taskList[location] ?: return
+        task.cancel()
+        taskList.remove(location)
     }
 }
